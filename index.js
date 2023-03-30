837b7c237d29de51ce6d947f5f971ebd85d79876
Rafael Sacramento <rsacramento@users.noreply.github.com>
2023-03-30 18:34:52 -0500

    Prepared to update AttributeDefinitions of corresponding tables

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
-const { is, capitalize } = require("@architect/inventory/src/lib")
+const { is, capitalize, errorFmt } = require("@architect/inventory/src/lib")
 
 const unique = objArray => [...new Set(objArray.map(i => Object.keys(i)[0]))]
-// TODO: Update AttributeDefinitions
+
 module.exports = {
 	deploy: {
-		start: async ({ arc, cloudformation, dryRun, inventory, stage }) => {
+		start: async ({ arc, cloudformation }) => {
 			let cfn = cloudformation
 
 			const localIndexes = arc["tables-local-indexes"]
 			if (!Array.isArray(localIndexes) || !localIndexes.length) return cloudformation
 
-			const arcTables = arc.tables
-
-			if (!Array.isArray(arcTables) || !unique(localIndexes).every(i => unique(arcTables).includes(i))) {
+			if (!Array.isArray(arc.tables) || !unique(localIndexes).every(i => unique(arc.tables).includes(i))) {
 				throw ReferenceError(`Specifying @tables-local-indexes requires specifying corresponding @tables`)
 			}
 
-			arcTables.forEach(table => {
+			// Loop thru manifest tables
+			arc.tables.forEach(table => {
 				const name = Object.keys(table).pop()
-
 				const tableName = name
 					?.split(/[-._]/)
 					.map(p => capitalize(p))
 					.join("")
 					.concat("Table")
 
-				const lsiProperty = localIndexes
-					.filter(arcLSI => Object.keys(arcLSI).pop() === name)
-					.reduce((prop, arcLSI) => {
-						const pk = cfn.Resources[tableName].Properties.KeySchema.filter(k => k.KeyType === "HASH")[0].AttributeName
-						let lsiTemplate = {
+				const tableProperties = cfn.Resources[tableName].Properties
+
+				// Reduce Local Indexes from manifest into CloudFormation DynamoDB properties
+				cfn.Resources[tableName].Properties = localIndexes
+					.filter(localIndex => Object.keys(localIndex).pop() === name)
+					.reduce((lsiProps, localIndex, i) => {
+						const pk = tableProperties.KeySchema.filter(k => k.KeyType === "HASH")[0].AttributeName
+						const lsiTemplate = {
 							IndexName: undefined,
 							KeySchema: [
 								{
@@ -46,46 +47,63 @@ module.exports = {
 							},
 						}
 
-						const lsiEntries = Object.entries(arcLSI[name])
-						const lsi = lsiEntries.reduce((cfnLSI, entry) => {
-							switch (entry[0]) {
-								case "name":
-									cfnLSI.IndexName = entry[1]
-									break
-								case "projection":
-									if (entry[1] === "keys") {
-										cfnLSI.Projection.ProjectionType = "KEYS_ONLY"
-									} else if (entry[1] === "all") {
-										cfnLSI.Projection.ProjectionType = "ALL"
-									} else {
-										cfnLSI.Projection.ProjectionType = "INCLUDE"
-										cfnLSI.Projection.NonKeyAttributes = Array.isArray(entry[1]) ? entry[1] : entry[1].split(" ")
-									}
-									break
-								default:
-									if (is.primaryKey(entry[1]) && entry[1] !== pk) {
-										throw ReferenceError(
-											`The partition key of a Local Secondary Index must be the same of the base table (${pk}). It cannot be ${entry[0]}.`
-										)
-									}
-									if (is.sortKey(entry[1])) {
-										cfnLSI.KeySchema.map(key => (key.KeyType === "RANGE" ? (key.AttributeName = entry[0]) : null))
-									}
-							}
+						// Check if table has no Local Index
+						const hasLocalIndex = unique(localIndexes).includes(Object.keys(table)[0])
 
-							return cfnLSI
-						}, lsiTemplate)
+						// Reduce Local Indexes' attributes into CloudFormation LSI properties
+						const attribs = Object.entries(localIndex[name])
+						return !hasLocalIndex
+							? lsiProps
+							: attribs.reduce(
+									(cfnProps, attr, seq) => {
+										let cfnLSI = seq === 0 ? lsiTemplate : cfnProps.LocalSecondaryIndexes[i]
 
-						lsi.IndexName ??= `${pk}-${lsi.KeySchema.filter(k => k.KeyType === "RANGE")[0].AttributeName}-index`
+										switch (attr[0]) {
+											case "name":
+												cfnLSI.IndexName = attr[1]
+												break
+											case "projection":
+												if (attr[1] === "keys") {
+													cfnLSI.Projection.ProjectionType = "KEYS_ONLY"
+												} else if (attr[1] === "all") {
+													cfnLSI.Projection.ProjectionType = "ALL"
+												} else {
+													cfnLSI.Projection.ProjectionType = "INCLUDE"
+													cfnLSI.Projection.NonKeyAttributes = Array.isArray(attr[1]) ? attr[1] : attr[1].split(" ")
+												}
+												break
+											default:
+												if (is.sortKey(attr[1])) {
+													cfnLSI.KeySchema.map(key => (key.KeyType === "RANGE" ? (key.AttributeName = attr[0]) : null))
+												} else if (is.primaryKey(attr[1]) && attr[1] !== pk) {
+													throw ReferenceError(
+														`The partition key of a Local Secondary Index must be the same of the base table (${pk}). It cannot be ${attr[0]}.`
+													)
+												} else {
+													// TODO: Update AttributeDefinitions
+												}
+										}
 
-						prop.push(lsi)
-						return prop
-					}, [])
+										// Build IndexName if one is not provided
+										const sk = cfnLSI.KeySchema.filter(k => k.KeyType === "RANGE")[0].AttributeName
+										cfnLSI.IndexName ??= `${pk}-${sk}-index`
 
-				cfn.Resources[tableName].Properties.LocalSecondaryIndexes = lsiProperty
+										// Update Cloudformation properties
+										if (seq === 0) {
+											cfnProps.LocalSecondaryIndexes ??= []
+											cfnProps.LocalSecondaryIndexes.push(cfnLSI)
+										} else {
+											cfnProps.LocalSecondaryIndexes[i] = cfnLSI
+										}
+
+										return cfnProps
+									},
+									{ ...lsiProps }
+							  )
+					}, tableProperties)
 			})
 
-			return cloudformation
+			return cfn
 		},
 	},
 }
